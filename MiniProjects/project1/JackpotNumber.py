e8313d90612714a8a0ab77995b4e1b84ed9d44d7
AnubhavKukreti1 <anubhavkukreti9@gmail.com>
2026-09-17 00:12:27 +0530

    your commit message

--- MiniProjects/project1/JackpotNumber.py
+++ MiniProjects/project1/JackpotNumber.py
@@ -1,3 +1,5 @@
+""" This is my project 1 for the mini projects. The project is to create a simple game where the user has to guess a randomly generated number between 1 and 100. The program will give hints if the guess is too low or too high, and will continue to prompt the user until they guess the correct number. Once the correct number is guessed, the program will congratulate the user. """
+
 import random 
 
 JackpotNumber = random.randint(1, 100)
@@ -16,5 +18,5 @@ while guess != JackpotNumber :
 else :
     print("Congratulations! You've guessed the Jackpot Number:", JackpotNumber)
 
-    
+
 
