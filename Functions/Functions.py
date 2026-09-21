0fd6c24440fdea3643e73a259794cd46fbf89577
AnubhavKukreti1 <anubhavkukreti9@gmail.com>
2026-09-21 12:53:41 +0530

    your commit message

---
+++ Functions/Functions.py
@@ -0,0 +1,26 @@
+
+# Write a func to print graphic era without using return type 
+
+def GEU():
+  print("Graphic Era")
+
+GEU()
+
+# write a function to find the sum of two numbers while making a function add use return tyoes
+
+a = int(input("Enter a number :"))
+b= int(input("Enter another number :"))
+
+def add():
+  return a + b 
+
+print(add())
+
+
+# write a funtion to print the cube of a number using parameter in a function and without return type
+
+def cube(num):
+  print(num * num * num)
+
+print(cube(3))
+
